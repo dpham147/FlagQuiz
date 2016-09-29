5f4e8b47e6ec7296f2e61460d48472e2df2a29b3
dpham147 <dannypham92@hotmail.com>
2016-09-29 01:03:18 -0700

    Fixed SettingsActivityFragment

--- app/src/main/java/edu/orangecoastcollege/cs273/dpham147/flagquiz/SettingsActivityFragment.java
+++ app/src/main/java/edu/orangecoastcollege/cs273/dpham147/flagquiz/SettingsActivityFragment.java
@@ -2,9 +2,6 @@ package edu.orangecoastcollege.cs273.dpham147.flagquiz;
 
 import android.os.Bundle;
 import android.preference.PreferenceFragment;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -12,7 +9,7 @@ import android.view.ViewGroup;
 public class SettingsActivityFragment extends PreferenceFragment {
 
     @Override
-    public View onCreate(Bundle bundle){
+    public void onCreate(Bundle bundle){
         super.onCreate(bundle);
         addPreferencesFromResource(R.xml.preferences);
     }
